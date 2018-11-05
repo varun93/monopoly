70ef63446c1d0cb11e69a216cd19877638f64573
SanjayThomas <sanjaythomas.mec@gmail.com>
2018-11-04 22:21:19 -0500

    adding static board. removing reduntant code

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ import config
 import dice
 import spaces
 
-
+logging.basicConfig(filename='monopoly.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -15,85 +15,95 @@ class Game:
 
     def __init__(self):
 
-        self.round = 0
-        self.board = {}
+        self.turn = 0
         self.dice = None
-        self.get_board(config.board_filename)
-
-    def get_board(self, board_file):
-        """
-        Create board game with properties from CSV file in board_file.
-        :param str board_file: Filename of CSV with board parameters
-        """
-
-        board_df = pd.read_csv(board_file)
-
-        monopoly_groups = {};
-            
-
-        for _, attributes in board_df.iterrows():
-
-            if attributes['class'] == 'Street':
-                
-                self.board[attributes['position']] = spaces.Street(attributes)
-                if (attributes['monopoly'] not in monopoly_groups):
-                    monopoly_groups[attributes['monopoly']] = []
-                monopoly_groups[attributes['monopoly']].append(self.board[attributes['position']])
-
-            if attributes['class'] == 'Railroad':
-                self.board[attributes['position']] = spaces.Railroad(attributes)
-                
-                if (attributes['monopoly'] not in monopoly_groups):
-                    monopoly_groups[attributes['monopoly']] = []
-                monopoly_groups[attributes['monopoly']].append(self.board[attributes['position']])
-
-            if attributes['class'] == 'Utility':
-                self.board[attributes['position']] = spaces.Utility(attributes)
-                if (attributes['monopoly'] not in monopoly_groups):
-                    monopoly_groups[attributes['monopoly']] = []
-                monopoly_groups[attributes['monopoly']].append(self.board[attributes['position']])
-
-            if attributes['class'] == 'Tax':
-                self.board[attributes['position']] = spaces.Tax(attributes)
-
-            if attributes['class'] == 'Chance':
-                chance_df = pd.read_csv(config.chance_filename)
-                self.board[attributes['position']] = spaces.Chance(chance_df)
-
-            if attributes['class'] == 'Chest':
-                comm_df = pd.read_csv(config.community_chest_filename)
-                self.board[attributes['position']] = spaces.Chest(comm_df)
-
-            if attributes['class'] in ['Jail', 'Idle']:
-                self.board[attributes['position']] = []
-
-        #Grouping together monopolies so that they can accessed more easily later
-        for monopoly in monopoly_groups:
-            for prop in monopoly_groups[monopoly]:
-                prop.monopoly_group_elements.extend([x for x in monopoly_groups[monopoly] if x.name!=prop.name])
-                
-        if config.verbose['board']:
-            self.print_board()
+        self.end_turn = False #Signal showing that it is ok to end the turn. Used to account for Double Throws.            
     
-    #Debugging Component            
-    def print_board(self):
-        for x in self.board:
-            if isinstance(self.board[x], spaces.Street):
-                logger.info("Position "+str(x));
-                logger.info("Name: "+self.board[x].name);
-                logger.info("Monopoly Group Elements: ");
-                for y in self.board[x].monopoly_group_elements:
-                    logger.info(y.name)
-                logger.info("")
-
+    #Function used to generate board in constants.py
+    #Run this again if changes are made on          
+    def generate_board_dictionary(self):
+        board_df = pd.read_csv(config.board_filename)
+        
+        monopoly_groups = {};
+        for _, attributes in board_df.iterrows():
+            if (attributes['monopoly'] not in monopoly_groups):
+                monopoly_groups[attributes['monopoly']] = []
+            monopoly_groups[attributes['monopoly']].append(attributes)
+        
+        headers = [key for key in board_df.dtypes.index]
+        print("{")
+        l=0
+        for _, attributes in board_df.iterrows():
+            
+            outer_comma = ""
+            if l is not len(board_df)-1:
+                outer_comma = ","   
+            
+            print(str(attributes['position']-1)+":{");
+            
+            k=0
+            hasMonopolies = ((attributes['class'] == 'Street') or (attributes['class'] == 'Railroad') or (attributes['class'] == 'Utility'))
+            for y in [z for z in attributes if z is not 'position']:
+                comma = ""
+                if (k is not len(headers)-1) or hasMonopolies:
+                    comma = ","
+                if isinstance(y, int):
+                    print("\""+headers[k]+"\":"+str(y)+comma)
+                else:
+                    print("\""+headers[k]+"\":\""+str(y)+"\""+comma)
+                k+=1
+            
+            if hasMonopolies:
+                groups = [x.position-1 for x in monopoly_groups[attributes['monopoly']] if x.name != attributes.name]
+                print("\"monopoly_group_elements\":"+str(groups))
+                    
+            
+            print("}"+outer_comma);
+            l+=1
+        print("}")
+        
+    """To reset dice for a new turn"""
     def pass_dice(self):
-
         self.dice = dice.Dice()
 
-    def update_round(self):
-
-        self.round += 1
-
-        if config.verbose['round']:
+    def update_turn(self):
+        self.turn += 1
+        if config.verbose['turn']:
             logger.info('\n')
-            logger.info('Starting round {round}...'.format(round=self.round))
+            logger.info('Starting turn {turn}...'.format(turn=self.turn))
+            
+            
+    """Phases"""
+    
+    """Phase 2: Dice Roll"""
+    def dice_roll(self,state):
+        self.pass_dice()
+        self.dice.roll()
+        
+        if self.dice.double_counter == 3:
+            send_player_to_jail(state)
+            #End current player's turn here
+            #Should there be a GoToJail state to let the player know?
+        else:
+            #Update player position
+            state.position += self.dice.roll_sum
+            #Next, perform square effect
+            #Preparation for next phase:
+            state.phase = 2 # effect phase
+            state.phase_properties = {}
+            #state.phase_properties.owned = 
+            
+    
+    
+    
+    def send_player_to_jail(self,state):
+        current_player = state.turn%2
+        state.position[current_player] = -1 #sending the player to jail
+        
+    
+    """Phase 3: BSTM"""
+    #def unowned_property(self):
+        
+g = Game()
+g.generate_board_dictionary()
+    
