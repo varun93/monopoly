46058a7b3c4abe1c046c1cc661262458ea2a4630
Pragesh Jagnani <pragesh4jagnani@gmail.com>
2018-11-08 22:51:55 -0500

    Added Agent Template Methods

--- agent.py
+++ agent.py
@@ -1,34 +1,42 @@
 class Agent:
+    def __init__(self, id):
+        self.id = id
+        pass
+    def getBMSTDecision(self, state):
+        # TODO:fill the template
+        pass
+    def respondTrade(self, state):
+        pass
 
-	def __init__(self, id):
-		self.id = id
-		pass
+    def buyProperty(self, state):
+        pass
 
-	def makeBMSTDecision(self,state):
-		# TODO:fill the template
-		pass
+    def auctionProperty(self, state):
+        pass
 
-	def parsePhase(self,state):
-		phaseNumber = state["phase"]
-		phasePayload = state["phase_payload"]
+    def receiveState(self, state):
+        pass
 
-		# how to distinguish between dice roll bmst and bmst before
-		if phaseNumber == 0:
-			handleBMSTDecison(state)
+    def parsePhase(self, state):
+        phaseNumber = state["phase"]
+        phasePayload = state["phase_payload"]
 
-		if phaseNumber == 3:
-			diceValue = phasePayload["dice_roll"]
-			currentPosition = state["player_position"][id]
-			# is mod 40 correct?
-			newPosition = (currentPosition + diceValue) % 40
-			propertyStatus = state["property_status"][newPosition]
-			
-			# retrieve the property
-			handleBMSTDecison(state)
-	
-	def jailDecision(self,state):
-		pass
-		
-	def run(self,state):
-		return {}
-		
+        # how to distinguish between dice roll bmst and bmst before
+        if phaseNumber == 0:
+            handleBMSTDecison(state)
+
+        if phaseNumber == 3:
+            diceValue = phasePayload["dice_roll"]
+            currentPosition = state["player_position"][id]
+            # is mod 40 correct?
+            newPosition = (currentPosition + diceValue) % 40
+            propertyStatus = state["property_status"][newPosition]
+
+            # retrieve the property
+            handleBMSTDecison(state)
+
+    def jailDecision(self, state):
+        pass
+
+    def run(self, state):
+        return {}
