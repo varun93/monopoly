d52ac7eb2649aa3ebf80f6fbfc97a28da9d5fd4c
varun93 <1hvarun@gmail.com>
2018-11-22 17:55:52 -0500

    jail bug fixed

--- react-user-interface/src/Board/PlayerInfo/index.js
+++ react-user-interface/src/Board/PlayerInfo/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 
-const PlayerInfo = ({ cash, debt }) => {
+const PlayerInfo = ({
+  playerOneCash,
+  playerTwoCash,
+  playerOneDebt,
+  playerTwoDebt
+}) => {
   return (
     <div className="center">
       <div className="player-one-info">
         <h2 className="label">Player One Info</h2>
-        <div>Cash : $30</div>
-        <div>Debt : $0</div>
+        <div>Cash : {playerOneCash}</div>
+        <div>Debt : {playerOneDebt}</div>
       </div>
       <div className="player-two-info">
         <h2 className="label">Player Two Info</h2>
-        <div>Cash : $50</div>
-        <div>Debt : $0</div>
+        <div>Cash : {playerTwoCash}</div>
+        <div>Debt : {playerTwoDebt}</div>
       </div>
     </div>
   );
